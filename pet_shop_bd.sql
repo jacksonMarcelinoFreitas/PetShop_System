1170f676e0c475dab75b7ed4ac630240be089851
Jackson Marcelino de Freitas <jacksonzitap.mc@gmail.com>
2023-05-24 13:51:18 -0300

    added tables on file .sql

--- pet_shop_bd.sql
+++ pet_shop_bd.sql
@@ -1,37 +1,113 @@
 CREATE TABLE CLIENTE(
 	cpfCliente varchar (50) PRIMARY KEY NOT NULL,
     nomeCliente varchar (20),
-    telefoneCliente varchar (20)
+    telefoneCliente varchar (20),
+    CONSTRAINT cliente_cpf UNIQUE (cpfCliente)
 );
 
 CREATE TABLE ENDERECO(
-	idEndereco int PRIMARY KEY NOT NULL AUTO_INCREMENT,
-    cidade varchar (50),
-    bairro varchar (50),
-    rua varchar (50),
-    numero int,
-    fk_cpfCliente varchar (20)
+	idEndereco INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    cidade VARCHAR (50),
+    bairro VARCHAR (50),
+    rua VARCHAR (50),
+    numero INT,
+    fk_cpfCliente VARCHAR (20),
+    FOREIGN KEY (fk_cpfCliente) REFERENCES CLIENTE(cpfCliente)
 );
 
-CREATE TABLE CLIENTEENDERECO(
-	fk_cliente_cpfCliente varchar (20),
-    fk_endereco_idEndereco int
+CREATE TABLE CLIENTEENDERECO (
+    fk_cliente_cpfCliente VARCHAR (20),
+    fk_endereco_idEndereco INT,
+    FOREIGN KEY (fk_cliente_cpfCliente) REFERENCES Cliente (cpfCliente),
+    FOREIGN KEY (fk_endereco_idEndereco) REFERENCES Endereco (idEndereco)
 );
 
-CREATE TABLE COMPRA(
-	fk_cliente_cpfCliente varchar (20),
-    fk_produto_cpfProduto int
-);
 
+CREATE TABLE COMPRA (
+	idCompra INT PRIMARY KEY AUTO_INCREMENT NOT NULL,
+	dataCompra DATETIME,
+    valorTotal DECIMAL,
+    fk_cliente_cpfCliente VARCHAR(20),
+    FOREIGN KEY (fk_cliente_cpfCliente) REFERENCES CLIENTE (cpfCliente)
+);
 
 CREATE TABLE PRODUTO(
-	nomeProduto varchar (20),
-    codProduto int,
-    valorProduto decimal,
-    descricaoProduto varchar (200)
+	idProduto INT PRIMARY KEY NOT NULL,
+	nomeProduto VARCHAR (20),
+    codProduto INT,
+    valorProduto DECIMAL,
+    descricaoProduto VARCHAR (200)
 );
 
-CREATE TABLE CLIENTEFUNCIONARIO(
-	fk_cliente_cpfCliente varchar (20),
-    fk_funcionario_cpfFuncionario varchar (20)
+CREATE TABLE FUNCIONARIO	(
+	cpfFuncionario VARCHAR (20) PRIMARY KEY NOT NULL,
+    nomeFuncionario VARCHAR(50),
+    cargoFuncionario VARCHAR(50)
 );
+
+CREATE TABLE CLIENTECOMPRA (
+    fk_cliente_cpfCliente varchar(20),
+    fk_compra_idCompra INT,
+    fk_produto_idProduto INT,
+    quantidade INT,
+    FOREIGN KEY (fk_cliente_cpfCliente) REFERENCES CLIENTE(cpfCliente),
+    FOREIGN KEY (fk_compra_idCompra) REFERENCES COMPRA(idCompra),
+    FOREIGN KEY (fk_produto_idProduto) REFERENCES PRODUTO(idProduto)
+);
+
+CREATE TABLE CLIENTEPRODUTO(
+	fk_cliente_cpfCliente VARCHAR(20),
+    fk_produto_idProduto INT,
+    FOREIGN KEY (fk_cliente_cpfCliente) REFERENCES CLIENTE (cpfCliente),
+    FOREIGN KEY (fk_produto_idProduto) REFERENCES PRODUTO (idProduto)
+);
+
+CREATE TABLE CLIENTEFUNCIONARIO (
+	data_atendimento DATETIME,
+    fk_cliente_cpfCliente VARCHAR(20),
+    fk_funcionario_cpfFuncionario VARCHAR(20),
+    FOREIGN KEY (fk_cliente_cpfCliente) REFERENCES CLIENTE (cpfCliente),
+    FOREIGN KEY (fk_funcionario_cpfFuncionario) REFERENCES FUNCIONARIO (cpfFuncionario)
+);
+
+
+
+
+CREATE TABLE SERVICO	(
+	idServico INT PRIMARY KEY AUTO_INCREMENT,
+    nomeServico VARCHAR(50),
+    valorServico DECIMAL
+);
+
+CREATE TABLE CLIENTESERVICO (
+    fk_cliente_cpfCliente VARCHAR(20),
+    fk_servico_idServico INT,
+    FOREIGN KEY (fk_cliente_cpfCliente) REFERENCES CLIENTE (cpfCliente),
+    FOREIGN KEY (fk_servico_idServico) REFERENCES SERVICO (idServico)
+);
+
+
+CREATE TABLE CLIENTEANIMAL (
+    idAnimal INT PRIMARY KEY,
+    nomeAnimal VARCHAR(50),
+    fk_cpfCliente VARCHAR(20),
+    FOREIGN KEY (fk_cpfCliente) REFERENCES CLIENTE (cpfCliente)
+);
+
+
+CREATE TABLE USUARIO (
+	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(50),
+    senha VARCHAR(20),
+    email VARCHAR(50),
+    cargo VARCHAR(50),
+    administrador BOOLEAN DEFAULT FALSE,
+    fk_cpfFuncionario VARCHAR (20),
+    CONSTRAINT uc_email UNIQUE (email),
+    FOREIGN KEY (fk_cpfFuncionario) REFERENCES FUNCIONARIO (cpfFuncionario)
+);
+
+
+
+
+
