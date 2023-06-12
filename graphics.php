3b9da2368cd631473d247d1e58574affd702863b
Jackson Marcelino de Freitas <jacksonzitap.mc@gmail.com>
2023-06-12 11:43:36 -0300

    using graphics

--- graphics.php
+++ graphics.php
@@ -4,14 +4,14 @@
         // Estabeleça a conexão usando a extensão PDO
         $conn = connection();
 
-        $cargo = "Vendedor";
+        $cargo = "vendedor";
         $sql = "SELECT COUNT(*) AS total_funcionarios FROM funcionario WHERE cargoFuncionario = :cargo";
         $stmt = $conn->prepare($sql);
         $stmt->execute(array(':cargo' => $cargo));
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
         $totalFuncionarios = $row['total_funcionarios'];
 
-        $cargo1 = "Atendente"; 
+        $cargo1 = "gerente"; 
         $sql = "SELECT COUNT(*) AS totalOutrosFuncionarios FROM funcionario WHERE cargoFuncionario = :cargo";
         $stmt = $conn->prepare($sql);
         $stmt->execute(array(':cargo' => $cargo1));
@@ -137,7 +137,7 @@
       <!-- Sidebar user panel (optional) -->
       <div class="user-panel mt-3 pb-3 mb-3 d-flex">
         <div class="image">
-          <img src="AdminLTE/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          <img src="./src/dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
           <a href="#" class="d-block">Alexander Pierce</a>
